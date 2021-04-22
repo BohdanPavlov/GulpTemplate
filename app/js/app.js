f5c6f79c1f44f18a90f7edae708ad34544f8e81b
BohdanPavlov <bogdanpavlov77@gmail.com>
2021-04-22 23:19:24 +0300

    added main slider

--- app/js/app.js
+++ app/js/app.js
@@ -2,4 +2,16 @@ $(document).ready(function () {
 	$('.header__burger').click(function (event) {
 		$('.header__burger,.header__menu').toggleClass('active');
 	});
+	const swiper = new Swiper('.main-slider', {
+		loop: true,
+
+		pagination: {
+			el: '.slider-pagination',
+		},
+
+		navigation: {
+			nextEl: '.slider__next',
+			prevEl: '.slider__prev',
+		},
+	});
 })
