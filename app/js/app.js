420101051696045d4b1d47a251f57d606c71e7f2
BohdanPavlov <bogdanpavlov77@gmail.com>
2021-04-29 23:00:24 +0300

    added tabs

--- app/js/app.js
+++ app/js/app.js
@@ -5,6 +5,10 @@ $(document).ready(function () {
 	const swiper = new Swiper('.main-slider', {
 		loop: true,
 
+		autoplay: {
+			delay: 3000,
+		},
+
 		pagination: {
 			el: '.slider-pagination',
 		},
@@ -13,5 +17,21 @@ $(document).ready(function () {
 			nextEl: '.slider__next',
 			prevEl: '.slider__prev',
 		},
+
 	});
-})
+
+	$(".projects__tabs").tabs();
+
+	const playButton = $("#video__play");
+	const content = $('.video__content');
+
+	playButton.on("click", function () {
+		if (video.paused == true) {
+			video.play();
+			content.addClass('hidden');
+		} else {
+			video.pause();
+			content.removeClass('hidden');
+		}
+	});
+});
